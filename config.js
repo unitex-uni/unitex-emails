7ae4420d7daf360807031a020d57efed737340cc
Brandon Palmeros <brandonpaldom@gmail.com>
2024-12-17 10:02:34 -0600

    Revert "feat: upgraded to Maizzle 5.0"
    This reverts commit 08fa509b94d5fa049b8565e7f900da098a09ed6d.

--- config.js
+++ config.js
@@ -3,19 +3,24 @@
 | Development config                      https://maizzle.com/docs/environments
 |-------------------------------------------------------------------------------
 |
-| This is the base configuration that Maizzle will use when you run commands
-| like `npm run build` or `npm run dev`. Additional config files will
-| inherit these settings, and can override them when necessary.
+| The exported object contains the default Maizzle settings for development.
+| This is used when you run `maizzle build` or `maizzle serve` and it has
+| the fastest build time, since most transformations are disabled.
 |
 */
 
 /** @type {import('@maizzle/framework').Config} */
-export default {
+module.exports = {
   build: {
-    content: ['emails/**/*.html'],
-    static: {
-      source: ['images/**/*.*'],
-      destination: 'images',
+    templates: {
+      source: 'src/templates',
+      destination: {
+        path: 'build_local',
+      },
+      assets: {
+        source: 'src/images',
+        destination: 'images',
+      },
     },
   },
 }
--- config.production.js
+++ config.production.js
@@ -3,23 +3,23 @@
 | Production config                       https://maizzle.com/docs/environments
 |-------------------------------------------------------------------------------
 |
-| This is the production configuration that Maizzle will use when you run the
-| `npm run build` command. Settings here will be merged on top of the base
-| `config.js`, so you only need to add the options that are changing.
+| This is where you define settings that optimize your emails for production.
+| These will be merged on top of the base config.js, so you only need to
+| specify the options that are changing.
 |
 */
 
 /** @type {import('@maizzle/framework').Config} */
-export default {
+module.exports = {
   build: {
-    output: {
-      path: 'build_production',
+    templates: {
+      destination: {
+        path: 'build_production',
+      },
     },
   },
-  css: {
-    inline: true,
-    purge: true,
-    shorthand: true,
-  },
+  inlineCSS: true,
+  removeUnusedCSS: true,
+  shorthandCSS: true,
   prettify: true,
 }
