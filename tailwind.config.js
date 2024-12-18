b50f05c615ff4bf28a25db90a814bcda266a8594
Brandon Palmeros <brandonpaldom@gmail.com>
2024-12-18 15:24:38 -0600

    feat: upgraded to Maizzle 6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [require('tailwindcss-preset-email')],
+  presets: [
+    require('tailwindcss-preset-email'),
+  ],
+  content: [
+    './components/**/*.html',
+    './emails/**/*.html',
+    './layouts/**/*.html',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'ui-sans-serif', 'system-ui', '-apple-system', '"Segoe UI"', 'sans-serif'],
+      },
+    },
+  },
 }
